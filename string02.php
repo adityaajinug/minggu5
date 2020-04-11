a73ddd6d2b478eda495daa51cd21a171bb153608
adityaajinug <56495110+adityaajinug@users.noreply.github.com>
2020-04-11 16:22:09 +0700

    array

--- string02.php
+++ string02.php
@@ -7,16 +7,17 @@ EOD;
 class foo {
     var $foo;
     var $bar;
-    function foo() {
+    function __construct() {
         $this->foo = 'Foo';
         $this->bar = array('Bar1', 'Bar2', 'Bar3');
+        
     }
 }
-$foo = new foo();
+$foo = new foo;
 $name = 'Achmatim';
 echo <<<EOT
     <u>$str</u><br>
-    My name is "<b>$name</b>". I am printing some <b>$foo->foo</b>. Now, I am printing some <b>{$foo->bar[1]}</b>. This should print a capital 'A': \x41
+My name is "<b>$name</b>". I am printing some <b>$foo->foo</b>. Now, I am printing some <b>{$foo->bar[1]}</b>. This should print a capital 'A': \x41
 EOT; 
 
 
